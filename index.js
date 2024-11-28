f44e846fcc5ad88bbbcbee624c51273167b08ae5
Neon68 <128075298+neon68dev@users.noreply.github.com>
2024-11-28 19:28:12 +0400

    V1.0

---
+++ index.js
@@ -0,0 +1,28 @@
+/** 
+* Encoder and decode from ASCII to HEX code
+* by neon68
+**/
+const codes = require('./codes.json');
+
+//* Source string, array of symbols from string and encoded/decoded string
+let str = "Hello, World! This code was wrote by neon68 (aka neon68dev)";
+let sym_arr = [];
+let code_str = [];
+
+//* If user writes 'decode' in arguments, code automatically makes mode 'decode'
+if (process.argv[2] == 'decode'){
+    //* Adding every symbol (without spaces) to the symbol array
+    sym_arr = str.split(' ');
+    for (let i = 0; i<sym_arr.length; i++) {
+        code_str += codes['decode'][sym_arr[i]];
+    }
+    console.log(code_str);
+}
+else {
+    //* Adding every symbol (including spaces) to the symbol array
+    sym_arr = str.split('');
+    for (let i = 0; i<sym_arr.length; i++) {
+        code_str += codes['encode'][sym_arr[i]] + ' ';
+    }
+    console.log(code_str);
+}
